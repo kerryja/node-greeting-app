ab911a3fcfecd57407a0704973b41b49e51d8928
Kerry Jackson <kerry.jackson108@gmail.com>
2019-05-14 20:13:51 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const greetings = require('./greeting.js');
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+const valedictions = require('./valediction.js');
+
+valedictions.valedictionIcelandic();
---
+++ valediction.js
@@ -0,0 +1,21 @@
+exports.valedictionEnglish = function () {
+    fs.appendFile('basic_node_answers.txt', 'Goodbye', (err) => {
+        if (err) throw err;
+    })
+    console.log('Goodbye was added to file');
+}
+
+exports.valedictionJapanese = function () {
+    fs.appendFile('basic_node_answers.txt', 'Sayonara', (err) => {
+        if (err) throw err;
+    })
+    console.log('Sayonara was added to file');
+}
+
+exports.valedictionSpanish = function () {
+    console.log('Adios');
+}
+
+exports.valedictionIcelandic = function () {
+    console.log('Bless');
+}
